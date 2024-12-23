2424f46bfe36f0ae038d91b1a819b1016b5b89e1
Sumir Seth <iamsumirseth@gmail.com>
2024-12-23 20:46:53 +0530

    motion

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,5 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', '@nuxt/image', '@nuxt/icon', '@nuxt/fonts'],
+  modules: ['@nuxtjs/tailwindcss', '@nuxt/image', '@nuxt/icon', '@nuxt/fonts', 'motion-v/nuxt'],
 })
