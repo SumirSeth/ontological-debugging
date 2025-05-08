d13d142becff82bc46e215334171bbac23d03db9
Sumir Seth <iamsumirseth@gmail.com>
2025-05-08 20:51:33 +0530

    add CORS headers to password retrieval endpoint

--- server/api/getPassKB.post.ts
+++ server/api/getPassKB.post.ts
@@ -1,6 +1,16 @@
 // read password from .env file
 
 export default defineEventHandler(async (event) => {
+
+  //CORS
+  setResponseHeaders(event, {
+    'Access-Control-Allow-Credentials': 'true',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+    'Access-Control-Allow-Headers': 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  })
+
+
   const body = await readBody(event)
 
   const correctPassword = process.env.PASSWORD
